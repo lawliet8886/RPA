bae216c62fea2ef87b1f2e6102c58c32622555d3
Gabriel da Silva Fernandes <124142123+lawliet8886@users.noreply.github.com>
2026-01-02 14:38:54 -0300

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-}
 
+    // ✅ NECESSÁRIO para Compose com Kotlin 2.x
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+}
